03eb9a81eb44528d00817eb383e9691c7134af72
Jeff Chiu <jwchiu82@gmail.com>
2021-09-27 18:30:20 -0700

    wire up googleauth

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "655828831140-7mfgpfcgpqlalkqah1h9c55o4qo19r8e.apps.googleusercontent.com",
+        scope: "email",
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
